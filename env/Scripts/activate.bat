0fb97c31364e719352aec7b9b8d1492eb656b226
kaloypedro64 <86336474+kaloypedro64@users.noreply.github.com>
2022-01-13 08:29:34 +0800

    change env path

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Program Development\Development\Github\MeterLab\env
+set VIRTUAL_ENV=C:\inetpub\wwwroot\MeterLab\env
 
 if not defined PROMPT set PROMPT=$P$G
 
