7f05bb4e1211c93bff51b30c762ded8f2acd6fbd
kaloypedro64 <86336474+kaloypedro64@users.noreply.github.com>
2022-01-18 10:24:21 +0800

    update settings

--- MeterLab/settings.py
+++ MeterLab/settings.py
@@ -32,7 +32,7 @@ ENCRYPT_KEY = 'cniyhb@tr-#mw)5)ta_ln*1g&0u4r9o&ryl+b=uo)apb(^@!)5'
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['192.168.1.2', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.2', 'localhost', '127.0.0.1','124.6.183.67']
 
 
 # Application definition
